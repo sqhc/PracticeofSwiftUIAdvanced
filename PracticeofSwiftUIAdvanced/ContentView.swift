c974c4444b1c639a64ac77b1650e62beeab747a1
shenqing <shenqin1@msu.edu>
2023-08-09 08:53:59 -0400

    Initial Commit

---
+++ PracticeofSwiftUIAdvanced/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeofSwiftUIAdvanced
+//
+//  Created by 沈清昊 on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticeofSwiftUIAdvanced/PracticeofSwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeofSwiftUIAdvancedApp.swift
+//  PracticeofSwiftUIAdvanced
+//
+//  Created by 沈清昊 on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeofSwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
